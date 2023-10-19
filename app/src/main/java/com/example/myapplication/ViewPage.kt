a683898adcd7760caf8be62cfaa4a0c3455ff0e2
Henkel <henke026@rangers.uwp.edu>
2023-10-19 00:02:24 -0500

    Adding Selection Page and View Page fragment as dummy begin start to make a stop selection possible

---
+++ app/src/main/java/com/example/myapplication/ViewPage.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ViewPage : Fragment(R.layout.fragment_view_page) {
+//TODO currently just a dummy and not used
+}
