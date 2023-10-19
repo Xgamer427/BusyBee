2a21e9970fb307f7fa5473c3d39da82ceae77930
Henkel <henke026@rangers.uwp.edu>
2023-10-18 23:11:15 -0500

    making some changes so the app could run on my computer (should not be a problem for other computers)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.busybee.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
