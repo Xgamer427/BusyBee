3f14b7c7d8c2feb6ef4e443dd3e3d813d3ba20d1
Xgamer427 <bmoffitt05@gmail.com>
2023-11-12 22:16:16 -0600

    Replacement for moffitt branch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,7 @@
 package com.example.myapplication
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.example.myapplication.data.Bus
-import com.example.myapplication.data.BusTrackerUiState
-import kotlinx.android.synthetic.main.fragment_selection_page.btnSelectStopSelection
-import kotlinx.android.synthetic.main.fragment_selection_page.tvStopSelection
-import kotlinx.coroutines.launch
 
 
 class MainActivity : FragmentActivity() {
@@ -19,8 +10,7 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
-
-
 
 }
